e2361250c1fa8fd90992bc5281c6fc406fda0643
lusteiger <leonardoxy7@gmail.com>
2020-11-21 21:56:05 +0800

    [xinyu lu] finish users.test.js

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,29 @@
+
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    const username = "leo";
+    const password = "pwd";
+    expect(await register(username, password)).toEqual({});
+    expect(axios.post).toHaveBeenCalledWith("/user", {
+      username,
+      password,
+    });
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockImplementation(() => false);
+
+    const username = "leo";
+    const password = "pwd";
+    await expect(() => register(username, password)).rejects.toThrow();
   });
 });
